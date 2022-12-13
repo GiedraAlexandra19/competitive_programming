d74fba41c7f3bf1e75a0dfde2b0e532540836ec8
Alex <83047121+GiedraAlexandra19@users.noreply.github.com>
2022-12-13 01:00:05 -0500

    strings

---
+++ strings uva/195.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+inline
+int letter(const char & c){
+	if(c >= 'A' && c <= 'Z')
+		return c - 'A';
+	return c - 'a';
+}
+
+bool compare(const char & a, const char & b){ //orden alfabetico
+	if(letter(a) == letter(b))
+		return a < b;
+	return letter(a) < letter(b);
+}
+
+int main(){
+	int n;
+	string str;
+
+	cin >> n;
+	while(n--){
+		cin >> str;
+		sort(str.begin(), str.end(), compare);  //ordenarlos
+
+		do{
+			cout << str << endl;
+		}
+		while(next_permutation(str.begin(), str.end(), compare));
+	}
+	return 0;
+}
